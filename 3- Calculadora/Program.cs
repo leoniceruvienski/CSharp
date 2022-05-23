3475dcd495811c47c90fd1fc04de240c30de183c
Leonice <leonice@pop-os.localdomain>
2022-05-23 10:54:20 -0300

    Soma de dois valores

--- 3- Calculadora/Program.cs
+++ 3- Calculadora/Program.cs
@@ -7,10 +7,25 @@ namespace Calculadora
         static void Main(string[] args)
         {
             Console.Clear();
-            Console.WriteLine("Primeirovalor: ");
-            float v1 = float.Parse(Console.ReadLine());
 
-            Console.WriteLine(v1);
+            Console.WriteLine("Primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine()); // Armazena o valor da variável digitada
+
+            Console.WriteLine("Segundo valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            float resultado = v1 + v2;
+
+            Console.WriteLine("O resultado da soma é " + resultado);
+            Console.WriteLine("O resultado da soma é " + (v1 + v2));
+            Console.WriteLine("O resultado da soma é " + v1 + v2); // se não por entre parentêses imprime o valor digitado, e não faz a soma.
+
+
+            Console.WriteLine($"O resultado da soma é {resultado}");
+            Console.WriteLine($"O resultado da soma é {v1 + v2}");
         }
     }
 }
+
