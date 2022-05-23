c0516c8ccd699eb516cb1c789d2fb60996ea818b
Leonice <leonice@pop-os.localdomain>
2022-05-23 09:55:26 -0300

    Calculadora

---
+++ 3- Calculadora/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Primeirovalor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine(v1);
+        }
+    }
+}
